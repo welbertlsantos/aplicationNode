67d9b14d280d3119916587bf0fb154c8e8c14e61
welbert santos <welbert.l.santos@gmail.com>
2019-05-09 00:02:51 -0300

    conclusão da tarefa

--- src/controller/somaController.js
+++ src/controller/somaController.js
@@ -3,12 +3,27 @@ exports.soma = async(req, res) => {
     let valor1 = req.query.valor1;
     let valor2 = req.query.valor2;
     let soma = await parseInt(valor1) + await parseInt(valor2);
-    let multiplica = await multiplicaFunc(soma);    
-    res.status(200).send ({soma: soma, multiplica: multiplica });
+    let multiplica = await multiplicaFunc(soma);
+    let divide = await divideFunc(multiplica);
+    let subtrai = await subtraiFunc(multiplica, soma);
+    res.status(200).send ({soma: soma, multiplica: multiplica, divide : divide, subtrai : subtrai });
 };
 
 
 function multiplicaFunc(soma) {
-    return parseInt(soma) * parseInt(2);
+    return new Promise((resolve) => {
+         resolve(parseInt(soma) * parseInt(2));
+        });
 }
 
+function divideFunc(multiplica) {
+    return new Promise((resolve) => {
+        resolve(parseInt(multiplica) / parseInt(2));
+    });
+}
+
+function subtraiFunc(multiplica, subtrai) {
+    return new Promise((resolve) =>{
+        resolve(parseInt(multiplica) - parseInt(subtrai));
+    });
+}
