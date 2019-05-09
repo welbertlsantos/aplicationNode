efcf4e113162f578e33394341f077ee20040b14c
welbert santos <welbert.l.santos@gmail.com>
2019-05-09 00:11:11 -0300

    Acerto código

--- src/controller/somaController.js
+++ src/controller/somaController.js
@@ -2,13 +2,18 @@ exports.soma = async(req, res) => {
     
     let valor1 = req.query.valor1;
     let valor2 = req.query.valor2;
-    let soma = await parseInt(valor1) + await parseInt(valor2);
+    let soma = await somaFunc(valor1, valor2);
     let multiplica = await multiplicaFunc(soma);
     let divide = await divideFunc(multiplica);
-    let subtrai = await subtraiFunc(multiplica, soma);
+    let subtrai = await subtraiFunc(divide, soma);
     res.status(200).send ({soma: soma, multiplica: multiplica, divide : divide, subtrai : subtrai });
 };
 
+function somaFunc (valor1, valor2) {
+    return new Promise((resolve) => {
+        resolve(parseInt(valor1) + parseInt(valor2));
+    })
+}
 
 function multiplicaFunc(soma) {
     return new Promise((resolve) => {
